924f6bf4d5f1e648642752b6a84edeb8d38a1f9a
b2etr <102886338+betterb2etr@users.noreply.github.com>
2022-11-05 18:26:46 +0900

    1105 18:26
    body parser까지 설치

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const mongoose = require("mongoose")
-const config = require("./config")
+const bodyParser = require('body-parser'); //1105
 
-mongoose.connect(config.mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>console.log("mongoDB connected..."));
+//application/x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 app.listen(port, ()=>console.log("listening on port: " + port +"\t http://localhost:"+ port));
 
 app.get('/',(req,res)=>{res.send("hello world!")});
+
+app.post('/register', (req, res) => {
+    //node react inflearn 강의7_ 4 : 12 참고
+// 필요한 정보들을 client에서 가져오면 그것들을 디비에 넣어줌. 
+
+
+})
