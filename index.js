10397e30120cc313c43b8eec0c5a3a4370446b91
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2022-11-06 19:16:43 +0900

    feat 데베에서 정보 불러오기 구현 완료
    * req에서 JSON형식으로 태그와 태그의 값이 저장된 Pivot을 받아서 해당 태그에 대해 검색한다.
    
    * 중복되는 결과에 대한 처리는 하지 않았다.
    * 비동기 함수 처리를 해주지 않았다.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const {User} = require('./models/User');
 const config = require('./config');
+const req = require("express/lib/request");
 
 
 app.listen(port, ()=>console.log("listening on port: " + port +"\t http://localhost:"+ port));
@@ -39,7 +40,30 @@ app.get("/", (req,res) => {
  });
 
  
-   
+app.get('/finddata',(req,res)=>{//요청에서 검색할 기준을 Pivot으로 받음
+    console.log("get data with keyword")
+    //req.body는 다음과 같음.
+    /*
+    {
+        "Pivot": "{\"<검색할 태그>\" : <태그의 값>}"
+    }
+    */
+    Pivot = JSON.parse(req.body.Pivot);
+    //전달받은 정보를 JSON형식으로 변환
+    
+    User.findOne(Pivot, function(err, User){
+        if(err){
+            console.log(err);
+        }else if(!User){
+            console.log("no such data");//데이터를 찾지 못하면 null을 전송함
+            res.json(null);
+        }else{
+            console.log(User);
+            res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
+        }
+    })
+    return res;
+})
 
 //application/x-ww-form-urlencoded 요렇게 생긴 데이터를 분석해서 가져올 수 있도록
 
