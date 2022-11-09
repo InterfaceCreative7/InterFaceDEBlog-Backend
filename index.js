97abed9d3d72c5bd5426e8485a9f80eafe536286
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2022-11-09 15:12:06 +0900

    feat: [#3] 완료
    * id값을 통해 찾은 데이터의 값을 바꾸어준다.
    * 아직 원하는 키의 값을 바꾸는 것은 구현하지 못했으며, 추후에 구현할 예정이다.
    
    Resolved #3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const {User} = require('./models/User');
 const config = require('./config');
 const req = require("express/lib/request");
+const { json } = require("body-parser");
 
 
 app.listen(port, ()=>console.log("listening on port: " + port +"\t http://localhost:"+ port));
@@ -51,30 +52,49 @@ let findAll = async (req, res)=> {//요청에서 검색할 기준을 Pivot으로
     }
     */
     //전달받은 정보를 JSON형식으로 변환
-    pi = "{"+ "\"" + req.body.key + "\"" + ':' + req.body.value + "}";
-    console.log(pi);
-    pi_JSON = JSON.parse(pi);
-    
-    User.find(pi_JSON, function(err, User){
-        if(err){
-            console.log(err);
-        }else if(!User){
-            console.log("no such data");//데이터를 찾지 못하면 null을 전송함
-            res.json(null);
-        }else{
-            console.log(User);
-            res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
-        }
-        //return res;
-    })
+
+    let pi = "{"+ "\"" + req.body.key + "\"" + ':' + req.body.value + "}";
+    if(req.body.key == undefined || req.body.value == undefined){//req에서 정확하게 입력받았는지 확인한다.
+        console.log("incorrect formet");//둘 중 하나라도 undefined, 즉 올바르지 못하다면 find를 실행하지 않는다.
+        res.json(null);
+    }
+    else{
+        pi_JSON = JSON.parse(pi);//JSON형식으로 바꾸어주고
+        User.find(pi_JSON, function(err, User){
+            if(err){
+                console.log(err);
+            }else if(!User){
+                console.log("no such data");//데이터를 찾지 못하면 null을 전송함
+                res.json(null);
+            }else{
+                console.log(User);
+                res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
+            }
+            //return res;
+        })
+    }
 }
 app.get('/findall',findAll);
 //application/x-ww-form-urlencoded 요렇게 생긴 데이터를 분석해서 가져올 수 있도록
 
 let modifyData = async (req, res)=>{
-    //const { id } = req.body._id;
+
+    let id = req.body._id;
+    let data;
+    User.updateOne({'_id':req._id}, {$set:{'age' : req.body.age}},(err, User)=>{
+        console.log(req.body.age);
+        if(err){
+            console.log(err);
+        }else if(!User){
+            console.log("no such data");//데이터를 찾지 못하면 null을 전송함
+           
+        }else{
+            console.log(User);
+            
+        }
+    });
     
-    User.findById({"_id": {"$oid" : "63677ab4a10c6ade6a6c3ba4"}}), function(err,User){
+    User.findById(id, (err, User)=>{
         if(err){
             console.log(err);
         }else if(!User){
@@ -84,8 +104,7 @@ let modifyData = async (req, res)=>{
             console.log(User);
             res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
         }
-    }
-    
+    })
     /*User.updateOne({ "_id" : id }, { "age" : req.age , "email" : req.email }, (err, User)=>{
         if(err){
             console.log(err);
@@ -99,4 +118,21 @@ let modifyData = async (req, res)=>{
     });*/
     
 }
-app.get('/modify', modifyData);
+app.get('/modify', (req,res)=>{
+    User.updateOne({"_id":req.body._id},{$set: {"age" : 91}},(err, User)=>{//설정된 태그의 값을 바꿈. req에서 태그와 값을 받는 것으로 바꾸어줄 예정
+        if(err){
+            console.log(err);
+        }else if(!User){
+            console.log("no such data");//데이터를 찾지 못하면 null을 전송함
+            res.json(null);
+        }else{
+            console.log(User);
+            res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
+        }
+    })
+})
+
+/*
+{"_id":{"$oid":"63677d7c7ec713cb3049cf4d"},"email":"interface@naver.com","name":"interface","age":{"$numberInt":"11"},"createdAt":{"$date":{"$numberLong":"1667726716115"}},"updatedAt":{"$date":{"$numberLong":"1667726716115"}},"__v":{"$numberInt":"0"}}
+*/
+
