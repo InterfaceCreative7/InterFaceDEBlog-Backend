514387785bbe4440aca9e2de50f78d19a8286269
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2022-11-10 16:57:00 +0900

    필요 없는 주석 삭제
    필요 없는 주석을 삭제함.

--- index.js
+++ index.js
@@ -87,25 +87,6 @@ let findAll = async (req, res)=> {//요청에서 검색할 기준을 Pivot으로
 app.post('/findall',findAll);
 //application/x-ww-form-urlencoded 요렇게 생긴 데이터를 분석해서 가져올 수 있도록
 
-/*
-app.post('/modify', (req,res)=>{//id와 수정할 정보의 키, 벨류를 받아 수정한다.
-    console.log(req.body.mod);
-    
-    User.updateMany({"_id":req.body._id},{$set: {"age" : 91}},(err, User)=>{//설정된 태그의 값을 바꿈. req에서 태그와 값을 받는 것으로 바꾸어줄 예정
-        if(err){
-            console.log(err);
-        }else if(!User){
-            console.log("no such data");//데이터를 찾지 못하면 null을 전송함
-            res.json(null);
-        }else{
-            console.log(User);
-            res.json(User);//찾았다면 해당 정보의 전체를 json 형식으로 전달함
-        }
-    })
-
-    
-})
-*/
 app.post('/modify', async (req,res)=>{
    
     /*
@@ -160,12 +141,4 @@ app.post('/delete', async (req,res)=>{//id값을 받아서 자료를 삭제함
 }
 })
 
-
-
-/*
-{"_id":{"$oid":"63677d7c7ec713cb3049cf4d"},"email":"interface@naver.com","name":"interface","age":{"$numberInt":"11"},"createdAt":{"$date":{"$numberLong":"1667726716115"}},"updatedAt":{"$date":{"$numberLong":"1667726716115"}},"__v":{"$numberInt":"0"}}
-{"_id":{"$oid":"6367881505ec1a3d7a2eff07"},"email":"interface@naver.com","name":"inaterface","age":{"$numberInt":"13"},"createdAt":{"$date":{"$numberLong":"1667729429566"}},"updatedAt":{"$date":{"$numberLong":"1667971198634"}},"__v":{"$numberInt":"0"}}
-{"_id":{"$oid":"636b9532f354f23a83798d3a"},"blogBody":"","imageUrl":"https://www.google.com","writedate":{"$date":{"$numberLong":"1667994930102"}},"title":"interface","writername":"interface","createdAt":{"$date":{"$numberLong":"1667994930108"}},"updatedAt":{"$date":{"$numberLong":"1667994930108"}},"__v":{"$numberInt":"0"}}
-{"_id":{"$oid":"6367881505ec1a3d7a2eff07"},"email":"interface@naver.com","name":"inaterface","age":{"$numberInt":"13"},"createdAt":{"$date":{"$numberLong":"1667729429566"}},"updatedAt":{"$date":{"$numberLong":"1668064898935"}},"__v":{"$numberInt":"0"},"writedate":{"$date":{"$numberLong":"1668044839535"}}}
-*/
 
